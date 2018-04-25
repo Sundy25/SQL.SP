db1e0fbe4ed3bfdd1286b53f52e8e19692841ad2
Elyor <levdeo@outlook.com>
2018-04-25 19:42:19 +0500

    clean

---
+++ src/SQL.SP.Demo/Employees.cs
@@ -0,0 +1,16 @@
+﻿namespace SQL.SP.Demo
+{
+    public class Employees
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID:{Id},Name:{Name},Salary:{Salary:F}";
+        }
+    }
+}
---
+++ src/SQL.SP.Demo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace SQL.SP.Demo
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var connetionString = "Data Source=localhost;Initial Catalog = master; User ID = sa; Password = web@1234;";
+
+            using (var db = new SqlConnection(connetionString))
+            {
+                //pager
+                Console.WriteLine("Pager 10 to 20 results:");
+                var employees = db.Query<Employees>("[dbo].[spGetEmployeesAll]", new { @from = 10, @to = 20 }, commandType: CommandType.StoredProcedure);
+                foreach (var item in employees.Take(5))
+                {
+                    Console.WriteLine(item);
+                }
+
+                //by id
+                Console.WriteLine("Get by id=1 result:");
+                var employee = db.QueryFirstOrDefault<Employees>("[dbo].[spGetEmployeeById]",new {@id=1},commandType:CommandType.StoredProcedure);
+                Console.WriteLine(employee);
+            }
+             
+            Console.ReadKey();
+        }
+    }
+}
